17db2cca046489e97e98a91cfd3f5de55e05e648
Siti Aulia Rahmatussyifa <sarsyifa@gmail.com>
2020-12-30 17:13:05 +0700

    remove unused code

--- app/src/main/cpp/watercalc-jni.cpp
+++ app/src/main/cpp/watercalc-jni.cpp
@@ -5,13 +5,6 @@
 #include <jni.h>
 
 extern "C"
-//JNIEXPORT jint JNICALL
-//Java_id_ac_ui_cs_mobileprogramming_sitiauliarahmatussyifa_watertime_CalculatorActivity_calculteTarget(
-//        JNIEnv *env, jobject thiz, jint weight) {
-//    jlong watereach = 30;
-//    return weight * watereach;
-//}
-extern "C"
 JNIEXPORT jint JNICALL
 Java_id_ac_ui_cs_mobileprogramming_sitiauliarahmatussyifa_watertime_CalculatorActivity_calculateTarget(
         JNIEnv *env, jobject thiz, jint weight) {
